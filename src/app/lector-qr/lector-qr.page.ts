628d13e92d3d16607dcb4a998b3db61438c2c8e8
Marcelo200221 <ma.darras@duocuc.cl>
2023-10-23 14:27:02 -0300

    Solo porque si

--- src/app/lector-qr/lector-qr.page.ts
+++ src/app/lector-qr/lector-qr.page.ts
@@ -25,7 +25,7 @@ export class LectorQRPage implements OnInit {
 
   @ViewChild('videoElement', { static: true }) videoElement: ElementRef | undefined;
   constructor(private storage: StorageService,private route: ActivatedRoute, private navCtrl: NavController) { 
-    this.isVisible = false;
+
     this.route.queryParams.subscribe(params => {
       this.nombre = params['username'];
     }) 
@@ -37,29 +37,24 @@ export class LectorQRPage implements OnInit {
   }
 
   
-
-  segmentChanged(event: any) {
-    // Detecta cuándo cambia el segmento y actualiza el valor de activeSegment
-    this.selectedTab = event.detail.value;
-  }
-
   async scanQRCode() {
-    const result = await BarcodeScanner.startScan();
-    if (result.hasContent) {
-      console.log('Contenido del código QR:', result.content);
-      this.qrResult = result.content; 
-      const datosEscaneados = this.qrResult.split(',');
-      this.storage.setvalue('Profesor', datosEscaneados);
-      this.navCtrl.navigateForward('/clase');
-      if(datosEscaneados){
-        this.isVisible = true;
+    try {
+      await BarcodeScanner.checkPermission()
+      const result = await BarcodeScanner.startScan();
+      if (result.hasContent) {
+        console.log('Contenido del código QR:', result.content);
+        BarcodeScanner.stopScan();
+        this.qrResult = result.content; 
+        const datosEscaneados = this.qrResult.split(',');
+        this.storage.setvalue('Profesor', datosEscaneados);
+        this.navCtrl.navigateForward('/clase');
+      } else {
+        // No se detectó ningún código QR
+        console.log('No se detectó ningún código QR');
       }
+    }catch (error){
+      console.error("No se pudo iniciar el escaner", error);
       
-      
-      
-    } else {
-      // No se detectó ningún código QR
-      console.log('No se detectó ningún código QR');
     }
   }
 
